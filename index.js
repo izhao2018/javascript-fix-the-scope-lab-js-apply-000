e90c202799a6e5a4c246be0235c242124fbd16df
persuasive-application-6896 <irenezhao16@gmail.com>
2019-06-11 23:04:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var funkyFunction = function() {
   }
 }
   var theFunk = funkyFunction();
-  console.log(theFunk);
+  theFunk();
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
